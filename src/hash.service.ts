e8eeb94451b86b923d8a2377a1030f57f851976c
memoduck <obsidian-memoduck-plugin@proton.me>
2025-03-31 17:29:39 +0300

    feat: add HashService

---
+++ src/hash.service.ts
@@ -0,0 +1,23 @@
+export interface IHashService {
+  sha256(data: string): Promise<string>;
+}
+
+export class HashService implements IHashService {
+  async sha256(data: string): Promise<string> {
+    // Convert the string to a byte array
+    const msgBuffer = new TextEncoder().encode(data);
+
+    // Compute the hash
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+    // Convert the hash to a byte array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    // Convert the byte array to a hexadecimal string
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, '0'))
+      .join('');
+
+    return hashHex;
+  }
+}
