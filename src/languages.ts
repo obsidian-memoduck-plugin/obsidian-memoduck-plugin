3feebdb278ba2a8458cec37a9786b538c147747b
memoduck <obsidian-memoduck-plugin@proton.me>
2025-03-31 17:27:08 +0300

    feat: add languages

---
+++ src/languages.ts
@@ -0,0 +1,16 @@
+export const languages = {
+  en: 'English',
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  'zh-CN': 'Mandarin Chinese',
+  hi: 'Hindi',
+  es: 'Spanish',
+  ar: 'Standard Arabic',
+  fr: 'French',
+  bn: 'Bengali',
+  pt: 'Portuguese',
+  ur: 'Urdu',
+  id: 'Indonesian',
+  de: 'German',
+  ja: 'Japanese',
+  uk: 'Ukrainian',
+};
